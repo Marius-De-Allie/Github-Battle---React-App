03a0566cac3bc4dbf4a066838efc7dd3987df90b
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-21 15:31:13 -0400

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Styles
+import './index.css';
+import Popular from './components/Popular';
+
+// Component peices.
+// 1. State
+// 2. Lifecycle
+// 3. UI - render method
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className='container'>
+                <Popular />
+            </div>
+        );
+    }
+};
+
+// ReactDOM.render takes 2 args:
+
+// 1. React Component./element
+// 2. where to render the element/component to
+
+ReactDOM.render(<App />, document.getElementById('app'));
