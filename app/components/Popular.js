5ca74276968df88cd49fddc40a7e68a087826f98
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-21 23:09:14 -0400

    Add onClick prop to button element within Popular component, value of prop set to this.updateLanguage function

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 
 class Popular extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedLanguage: 'All'
+        };
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+
+    updateLanguage(selectedLanguage) {
+        this.setState({
+            selectedLanguage
+        })
+    };
+
     render() {
         const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
         return (
             <ul className='flex-center'>
                 {languages.map(language => (
                     <li key={language}>
-                        <button className='btn-clear nav-link'>
+                        <button 
+                            className='btn-clear nav-link'
+                            onClick={() => this.updateLanguage(language)}
+                        >
                             {language}
                         </button>
                     </li>
                 ))}
+                <p>{this.state.selectedLanguage}</p>
             </ul>
         );
     }
