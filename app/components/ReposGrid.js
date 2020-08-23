bf031e4ed53bafc0661cf8f86fc8af1e102bc9c2
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-22 22:10:39 -0400

    Declare propTypes for ReposGrid component

--- app/components/ReposGrid.js
+++ app/components/ReposGrid.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa';
 
 const ReposGrids = ({ repos }) => console.log(repos)||(
@@ -18,12 +19,12 @@ const ReposGrids = ({ repos }) => console.log(repos)||(
                         alt={`Avatar for ${repo.owner.login}`}
                     />
                     <h2 className='center-text'>
-                        <a className='link' href={repo.html_url}>{repo.owner.login}</a>
+                        <a className='link' href={repo.html_url} target='_blank'title={`${repo.owner.login} git hub repo`}>{repo.owner.login}</a>
                     </h2>
                     <ul className="card-list">
                         <li>
                             <FaUser color='rgb(255, 191, 116)' size={22} />
-                            <a href={`https://github.com/${repo.owner.login}`}>{repo.owner.login}</a>
+                            <a href={`https://github.com/${repo.owner.login}`} target='_blank'>{repo.owner.login}</a>
                         </li>
                         <li>
                             <FaStar color='rgb(255, 215, 0)' size={22} />
@@ -43,5 +44,10 @@ const ReposGrids = ({ repos }) => console.log(repos)||(
         })}
     </ul>
 );
+
+// Component proptypes.
+ReposGrids.propTypes = {
+    repos: PropTypes.array.isRequired
+};
 
 export default ReposGrids;
