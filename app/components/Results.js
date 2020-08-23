9a7787c833e99c91e5c3e047bae61ba95ced46ec
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-23 18:21:34 -0400

    Add component state to Results component, state contains the following properties, winner, loser, error and loading

--- app/components/Results.js
+++ app/components/Results.js
@@ -3,6 +3,17 @@ import { battle } from '../utils/api';
 
 class Results extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            winner: null,
+            loser: null,
+            error: null,
+            loading: true
+        }
+    }
+
     componentDidMount() {
         battle([this.props.playerOne, this.props.playerTwo])
         .then((players) =>console.log(players))
