b207aec6df8cc87867b228599b9077a166999c0a
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-24 23:29:47 -0400

    Create class based Loader component

---
+++ app/components/Loader.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Loader extends React.Component {
+    constructor(props) {
+        super(prop);
+
+        this.state = {
+            content: props.text
+        }
+    }
+
+    componentDidMount() {
+        const { text, speed } = this.prop;
+        this.interval = window.setInterval(() => {
+            this.state.content === text + '...' ?
+            this.setState({
+                content: text
+            }) :
+            this.setState(prevState => ({
+                content: prevState.content + '.'
+            }))
+        }, speed)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        return (
+            <p>{this.state.content}</p>
+        );
+    }
+};
+
+Loader.defaultProps = {
+    text: 'loading',
+    speed: 300
+};
+
+Loader.propTypes = {
+    text: PropTypes.string.isRequired,
+    speed: PropTypes.number.isRequired
+};
+
+export default Loader;
