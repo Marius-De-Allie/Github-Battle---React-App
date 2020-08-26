aa3a0de4eee2fc573258591de1b595c65c211a52
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-26 12:26:31 -0400

    Add react-router-dom NavLink components for Popular and Battle pagesto Nav component render method

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import ThemeContext from '../contexts/theme';
 
 const Nav = () => (
     <ThemeContext.Consumer>
         {({ theme, toggleTheme }) => (
             <nav className='row space-between'>
+                <ul className='row nav'>
+                    <li>
+                        <NavLink exact to='/' className='nav-link' activeClassName='active-nav'>Popular</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/battle' className='nav-link' activeClassName='active-nav'>Battle</NavLink>
+                    </li>
+                </ul>
                 <button
                     className='btn-clear'
                     style={{fontSize: 30}}
