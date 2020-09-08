cc7123069e5a368d8960fbd0f78a726b736be2a0
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-08 17:13:49 -0400

    Refactor PlayerPreview component to use useContext hook in place of ThemeContext.Consumer component

--- app/components/PlayerPreview.js
+++ app/components/PlayerPreview.js
@@ -1,36 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FaTimesCircle } from 'react-icons/fa';
 import ThemeContext from '../contexts/theme';
 
-const PlayerPreview = ({ username, onReset, label }) => (
-    <ThemeContext.Consumer>
-        {({ theme }) => (
-            <div className='column player'>
-                <h3 className='player-label'>{label}</h3>
-                <div className={`row bg-${theme}`}>
-                    <div className='player-info'>
-                        <img 
-                            className='avatar-small' 
-                            src={`https://github.com/${username}.png?size=200`} 
-                            alt={`Avatar for ${username}`} 
-                        />
-                        <a
-                            href={`https://github.com/${username}`}
-                            className='link'
-                        >
-                            {username}
-                        </a>
-                        <button 
-                            className='btn-clear flex-center'
-                            onClick={onReset}
-                        >
-                            <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
-                        </button>
-                    </div>
+const PlayerPreview = ({ username, onReset, label }) => {
+    const theme = useContext(ThemeContext);
+
+    return (
+        <div className='column player'>
+            <h3 className='player-label'>{label}</h3>
+            <div className={`row bg-${theme}`}>
+                <div className='player-info'>
+                    <img 
+                        className='avatar-small' 
+                        src={`https://github.com/${username}.png?size=200`} 
+                        alt={`Avatar for ${username}`} 
+                    />
+                    <a
+                        href={`https://github.com/${username}`}
+                        className='link'
+                    >
+                        {username}
+                    </a>
+                    <button 
+                        className='btn-clear flex-center'
+                        onClick={onReset}
+                    >
+                        <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
+                    </button>
                 </div>
             </div>
-        )}
-    </ThemeContext.Consumer>
-);
-
+        </div>
+    )
+};
+   
 export default PlayerPreview;
