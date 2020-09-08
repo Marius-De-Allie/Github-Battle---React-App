a35faecb4568289eb1fc8f6d6899cac9b152ad41
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-08 19:47:05 -0400

    Refactor Card component to use useContext hook in place of ThemeContext.Consumer component and render prop

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import ThemeContext from '../contexts/theme';
 
-const Card = ({ header, avatar, name, subHeader, href, children }) => (
-    <ThemeContext.Consumer>
-        {({ theme }) => (
-            <div className={`card bg-${theme}`}>
-                <h4 className='header-lg center-text'>
-                    {header}
-                </h4>
-                <img 
-                    className='avatar' 
-                    src={avatar} 
-                    alt={`Avatar for ${name}`} 
-                />
-                <h4 className='center-text'>
-                    {subHeader}
-                </h4>
-                <h2 className='center-text'>
-                    <a className='link' href={href} target='_blank'>
-                        {name}
-                    </a>
-                </h2>
-                {children}
-            </div>
-        )}
-    </ThemeContext.Consumer>
-);
+const Card = ({ header, avatar, name, subHeader, href, children }) => {
+    const theme = useContext(ThemeContext);
+
+    return (
+        <div className={`card bg-${theme}`}>
+            <h4 className='header-lg center-text'>
+                {header}
+            </h4>
+            <img 
+                className='avatar' 
+                src={avatar} 
+                alt={`Avatar for ${name}`} 
+            />
+            <h4 className='center-text'>
+                {subHeader}
+            </h4>
+            <h2 className='center-text'>
+                <a className='link' href={href} target='_blank'>
+                    {name}
+                </a>
+            </h2>
+            {children}
+        </div>
+    );
+};
 
 // Card proptypes.
 Card.propTypes = {
