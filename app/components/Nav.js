aad5a05971781d50775bd0b50c2ab652cd7f46af
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-08 13:35:34 -0400

    Refactor Nav component to use useContext hook in place of ThemeContext.Consumer component and render prop

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,31 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import ThemeContext from '../contexts/theme';
 
-const Nav = () => (
-    <ThemeContext.Consumer>
-        {({ theme, toggleTheme }) => (
-            <nav className='row space-between'>
-                <ul className='row nav'>
-                    <li>
-                        <NavLink exact to='/' className='nav-link' activeClassName='active-nav'>Popular</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/battle' className='nav-link' activeClassName='active-nav'>Battle</NavLink>
-                    </li>
-                </ul>
-                <button
-                    className='btn-clear'
-                    style={{fontSize: 30}}
-                    onClick={toggleTheme}
-                >
-                    {theme === 'light' ? '🔦' : '💡'}
-                </button>
-            </nav>
-        )}
-    
-    </ThemeContext.Consumer>
+const Nav = ({ toggleTheme} ) => {
+    const theme = useContext(ThemeContext);
 
-);
+    return (
+        <nav className='row space-between'>
+            <ul className='row nav'>
+                <li>
+                    <NavLink exact to='/' className='nav-link' activeClassName='active-nav'>Popular</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/battle' className='nav-link' activeClassName='active-nav'>Battle</NavLink>
+                </li>
+            </ul>
+            <button
+                className='btn-clear'
+                style={{fontSize: 30}}
+                onClick={toggleTheme}
+            >
+                {theme === 'light' ? '🔦' : '💡'}
+            </button>
+        </nav>
+    );
+};
 
 export default Nav;
