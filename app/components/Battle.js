d44006e305fe268c9cf849cf452767bd5e221384
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-08 17:09:12 -0400

    Refactor Battle component from a class component to function component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,35 +1,23 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Instructions from './BattleInstructions';
 import PlayerInput from './PlayerInput';
 import PlayerPreview from './PlayerPreview';
 import Results from './Results';
+import { render } from 'react-dom';
 
-class Battle extends Component {
+const Battle = () => {
+    const [ playerOne, setPlayerOne ] = useState(null);
+    const [ playerTwo, setPlayerTwo ] = useState(null);
 
-    state = {
-        playerOne: null,
-        playerTwo: null,
-        battle: false
-    }
+    const handleSubmit = (id, player) => id === 'playerOne' 
+        ? setPlayerOne(player) 
+        : setPlayerTwo(player);
 
-    handleSubmit = (id, player) => {
-        this.setState({
-            [id]: player
-        })
-    }
+    const handleReset = (id) => id === 'playerOne' 
+        ? setPlayerOne(null) 
+        : setPlayerTwo(null);
 
-    handleReset = (id) => {
-        // Reset playerOne or PlayerTwo state to null
-        this.setState({
-            [id]: null
-        });
-    }
-
-    render() {
-        const { playerOne, playerTwo } = this.state; 
-
-      
         return (
             <Fragment>
                 <Instructions />
@@ -38,33 +26,33 @@ class Battle extends Component {
                     <div className='row space-around'>
                         {!playerOne ? 
                             <PlayerInput 
-                                onSubmit={player => this.handleSubmit('playerOne',player)} 
+                                onSubmit={(player) => handleSubmit('playerOne', player)} 
                                 label='Player One'
                             /> : 
                             <PlayerPreview 
                                 label='Player One' 
-                                onReset={() => this.handleReset('playerOne')} 
+                                onReset={() => handleReset('playerOne')} 
                                 username={playerOne}
                             /> 
                         }
                         {!playerTwo ? 
                             <PlayerInput 
-                                onSubmit={player => this.handleSubmit('playerTwo', player)} 
+                                onSubmit={(player) => handleSubmit('playerTwo', player)} 
                                 label='Player Two'
                             /> : 
                             <PlayerPreview 
                                 label='Player Two' 
-                                onReset={() => this.handleReset('playerTwo')} 
+                                onReset={() => handleReset('playerTwo')} 
                                 username={playerTwo}
                             /> 
                         }
                     </div>
                     {playerOne && playerTwo && 
                         <Link
-                         to={{
-                             pathname: '/battle/results',
-                             search: `?playerOne=${playerOne}&playerTwo=${playerTwo}`
-                         }}
+                        to={{
+                            pathname: '/battle/results',
+                            search: `?playerOne=${playerOne}&playerTwo=${playerTwo}`
+                        }}
                         >
                             Battle
                         </Link>
@@ -72,7 +60,77 @@ class Battle extends Component {
                 </div>
             </Fragment>
         );
-    }
-};
+    };
+/* */
+
+// class Battle extends Component {
+
+//     state = {
+//         playerOne: null,
+//         playerTwo: null,
+//         battle: false
+//     }
+
+//     handleSubmit = (id, player) => {
+//         this.setState({
+//             [id]: player
+//         })
+//     }
+
+//     handleReset = (id) => {
+//         // Reset playerOne or PlayerTwo state to null
+//         this.setState({
+//             [id]: null
+//         });
+//     }
+
+//     render() {
+//         const { playerOne, playerTwo } = this.state; 
+
+      
+//         return (
+//             <Fragment>
+//                 <Instructions />
+//                 <div className='players-container'>
+//                     <h1 className='center-text header-lg'>Players</h1>
+//                     <div className='row space-around'>
+//                         {!playerOne ? 
+//                             <PlayerInput 
+//                                 onSubmit={player => this.handleSubmit('playerOne',player)} 
+//                                 label='Player One'
+//                             /> : 
+//                             <PlayerPreview 
+//                                 label='Player One' 
+//                                 onReset={() => this.handleReset('playerOne')} 
+//                                 username={playerOne}
+//                             /> 
+//                         }
+//                         {!playerTwo ? 
+//                             <PlayerInput 
+//                                 onSubmit={player => this.handleSubmit('playerTwo', player)} 
+//                                 label='Player Two'
+//                             /> : 
+//                             <PlayerPreview 
+//                                 label='Player Two' 
+//                                 onReset={() => this.handleReset('playerTwo')} 
+//                                 username={playerTwo}
+//                             /> 
+//                         }
+//                     </div>
+//                     {playerOne && playerTwo && 
+//                         <Link
+//                          to={{
+//                              pathname: '/battle/results',
+//                              search: `?playerOne=${playerOne}&playerTwo=${playerTwo}`
+//                          }}
+//                         >
+//                             Battle
+//                         </Link>
+//                     }
+//                 </div>
+//             </Fragment>
+//         );
+//     }
+// };
 
 export default Battle;
